8065bd4315931cef971d5ddafd9aad603a609476
Icecream <981464828@qq.com>
2019-12-05 15:47:45 +0800

    init

---
+++ log.go
@@ -0,0 +1,39 @@
+package goutils
+
+import (
+	"log"
+	"os"
+)
+
+func LogInfo(logFilePath string, msg string) {
+	logFile, err := os.OpenFile(logFilePath, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0666)
+	defer logFile.Close()
+	if err != nil {
+		log.Fatalln("open file error !")
+	}
+	// 创建一个日志对象
+	debugLog := log.New(logFile, "", log.LstdFlags)
+	debugLog.Println(msg)
+}
+
+func FileLogPrintln(logFilePath string, v ...interface{}) {
+	logFile, err := os.OpenFile(logFilePath, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0666)
+	defer logFile.Close()
+	if err != nil {
+		log.Fatalln("open file error !")
+	}
+	// 创建一个日志对象
+	debugLog := log.New(logFile, "", log.LstdFlags)
+	debugLog.Println(v...)
+}
+
+func FileLogPrintf(logFilePath string, format string, v ...interface{}) {
+	logFile, err := os.OpenFile(logFilePath, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0666)
+	defer logFile.Close()
+	if err != nil {
+		log.Fatalln("open file error !")
+	}
+	// 创建一个日志对象
+	debugLog := log.New(logFile, "", log.LstdFlags)
+	debugLog.Printf(format, v...)
+}
