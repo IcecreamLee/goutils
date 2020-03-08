d59e47a8866e862b68ba653d3b568ae9721596dd
Icecream <981464828@qq.com>
2020-03-09 00:28:18 +0800

    update

--- log.go
+++ log.go
@@ -5,6 +5,17 @@ import (
 	"os"
 )
 
+func NewLogger(logFilePath string, prefix string) *log.Logger {
+	logFile, err := os.OpenFile(logFilePath, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0666)
+	defer logFile.Close()
+	if err != nil {
+		log.Fatalln("open file error !")
+	}
+	// 创建一个日志对象
+	logger := log.New(logFile, prefix, log.LstdFlags)
+	return logger
+}
+
 func LogInfo(logFilePath string, msg string) {
 	logFile, err := os.OpenFile(logFilePath, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0666)
 	defer logFile.Close()
@@ -17,7 +28,7 @@ func LogInfo(logFilePath string, msg string) {
 }
 
 func FileLogPrintln(logFilePath string, v ...interface{}) {
-	logFile, err := os.OpenFile(logFilePath, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0666)
+	logFile, err := os.OpenFile(logFilePath, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0777)
 	defer logFile.Close()
 	if err != nil {
 		log.Fatalln("open file error !")
